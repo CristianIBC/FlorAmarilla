05cfe52bfe5679f52536620a315adc739a63182c
Cristian Bravo <49567999+CristianIBC@users.noreply.github.com>
2024-12-04 11:22:44 -0600

    Add files via upload

---
+++ Abraham/main.js
@@ -0,0 +1,6 @@
+onload = () =>{
+        document.body.classList.remove("container");
+        document.getElementById("cancion").play();
+        document.getElementById("imagenIvan").style.width= "100%";
+        document.getElementById("imagenIvan").style.height= "100%";
+};
